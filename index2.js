d2a10d874ad1d5cd2d146065298bf05f07f9a80e
Jin._.0 <75020232+cjo70241@users.noreply.github.com>
2022-08-09 20:03:51 +0900

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,18 @@
+let apple_price = 10;
+
+if (apple_price >= 10) {
+  console.log("very expensive :)");
+
+} else if(apple_price < 5) {
+  console.log("very cheap :)");
+
+} else {
+  console.log("nice!");
+}
+
+let age = 10;
+
+if (age < 10)
+console.log("young!");
+else
+console.log("old!");
