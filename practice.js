b081fc47792bf9d39e292dc63e26fde3acfc3190
Jin._.0 <75020232+cjo70241@users.noreply.github.com>
2022-08-14 23:48:54 +0900

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,18 @@
+//두 정수 입력받아 가장 큰 값 출력해주는 함수
+function MAX(x, y) {
+  if(x > y) {
+    return x;
+  }
+  else {
+    return y;
+  }
+
+
+}
+
+console.log(MAX(0, 3));
+
+console.log(MAX(-1, 5));
+
+console.log(MAX(100, 7));
+
